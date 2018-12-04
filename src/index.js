688b1a1ac9af7556041064913e6a7fefd18193d5
Shayan Mahmoudi <iamsha4yan@gmail.com>
2018-12-04 23:45:36 +0330

    Add Gestalt and Bootstrap-iso CSS Global Path

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './styles/gestalt.css';
+import './styles/bs/css/bootstrap-iso.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
